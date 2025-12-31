b475902009abf02bf050680de17f5be87da51f0d
MHL-LewisHamilton <lewis.hamilton@hart-ltd.com>
2025-12-31 15:02:53 +0000

    Added a pie chart component that reacts to tasks being completed.

--- src/app/pages/overview/overview.ts
+++ src/app/pages/overview/overview.ts
@@ -1,16 +1,34 @@
 import { Component } from '@angular/core';
 import { TableList } from '../../components/table-list/table-list';
+import { Item } from '../../models/item.model';
+import { TaskPieComponent } from '../../components/task-pie/task-pie';
 import { HttpClientModule } from '@angular/common/http';
+import { TaskService, Task } from '../../services/task.service';
 
 @Component({
   selector: 'app-overview',
-  imports: [TableList, HttpClientModule],
+  imports: [TableList, HttpClientModule, TaskPieComponent],
   templateUrl: './overview.html',
   styleUrl: './overview.scss',
 })
 export class Overview {
+  public items: Array<Item> = [];
 
-  constructor() {
+  constructor(private taskService: TaskService) {
+    this.loadTasks();
   }
 
+  loadTasks() {
+    this.taskService.getTasks().subscribe((data: Task[]) => {
+      this.items = data.map(t => new Item(t.name, new Date(t.deadline), t.complete, t.id));
+    });
+  }
+
+  get completeCount() {
+    return this.items.filter(i => i.complete).length;
+  }
+
+  get incompleteCount() {
+    return this.items.filter(i => !i.complete).length;
+  }
 }
